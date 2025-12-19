7a1b8c3869f6d173805fcf91db0ac5f00a64b951
Евгений <titanozxc@gmail.com>
2025-12-19 04:33:41 +0300

    Создание кастомной коллекции слайдов - SlaidKolleksiya.java - интерфейс коллекции - SlaidSpisok.java - реализация на двусвязном списке с внутренним итератором
    Коллекция предоставляет:
    1. Гибкое хранение слайдов вместо массива
    2. Быстрое изменение порядка (pomenyatMesta, peremestit)
    3. Итерацию в обоих направлениях
    4. Эффективные операции добавления/удаления

--- src/main/java/kollektsii/SlaidKolleksiya.java
+++ src/main/java/kollektsii/SlaidKolleksiya.java
@@ -1,4 +1,35 @@
 package kollektsii;
 
-public class SlaidKolleksiya {
+import model.Slaid.Slaid;
+import java.util.Iterator;
+
+/**
+ * Интерфейс кастомной коллекции слайдов
+ * Вместо обычного массива - гибкая структура
+ */
+public interface SlaidKolleksiya extends Iterable<Slaid> {
+
+    // Основные операции
+    void dobavit(Slaid slaid);
+    void dobavit(int index, Slaid slaid);
+    void udalit(Slaid slaid);
+    void udalit(int index);
+    Slaid poluchit(int index);
+    int razmer();
+    boolean pusto();
+    void ochistit();
+
+    // Специальные операции для слайдов
+    void pomenyatMesta(int index1, int index2);
+    void peremestit(int otIndex, int kIndex);
+    void obratniyPoryadok();
+
+    // Поиск
+    int naytiIndex(Slaid slaid);
+    boolean soderzhit(Slaid slaid);
+
+    // Итераторы
+    Iterator<Slaid> iterator();
+    Iterator<Slaid> iteratorSkonca();
+    Iterator<Slaid> iteratorSOpredelennogoMesta(int startIndex);
 }
